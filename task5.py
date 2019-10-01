eb3d2beccc972cf801d3fe2631c9246bb5a9f48c
xmakss <xmakss2009@mail.ru>
2019-10-01 14:14:32 +0300

    Сделана 5ая задача Левенштейн

--- task5.py
+++ task5.py
@@ -6,14 +6,14 @@ def levenshatin(a, b):
         a, b = b, a
         n, m = m, n
 
-    row_now = range(n + 1)
-    for i in range(1, m + 1):
+    row_now = list(range(n + 1))
+    for i in range(m + 1):
         row_past, row_now = row_now, [i] + [0] * n
-        for j in range(1, n + 1):
+        for j in range(1, n+1):
             frst, scnd, thrd = row_past[j] + 1, row_now[j - 1] + 1, row_past[j - 1]
             if a[j - 1] != b[i - 1]:
                 thrd += 1
             row_now[j] = min(frst, scnd, thrd)
     return row_now[n]
 
-print(levenshatin("Электроника", "Математика"))
+print(levenshatin("Привет", "Притев"))
