13bb4d33bbb2f55a49498b552f635cf5593b8d36
xmakss <xmakss2009@mail.ru>
2019-10-06 18:06:04 +0300

    Добавил атрибуты классам

--- task3(03.10).py
+++ task3(03.10).py
@@ -38,6 +38,7 @@ class S_Verh(Flat):
 
 
 class Bez_Verh(Flat):
+
     pi = 3.14
 
     def info(self):
@@ -46,14 +47,23 @@ class Bez_Verh(Flat):
 
 class Circle(Bez_Verh):
 
+    Vpisan = False
+    Opisan = False
+
     def info(self):
         print("Круг, наследник плоских фигур без вершин")
+        if self.Vpisan == True:
+            print("Круг вписан  ")
+        if self.Opisan == True:
+            print("Круг описан  ")
 
-    def __init__(self,r,color):
+    def __init__(self, r, color, Vpisan = False, Opisan = False):
         print()
         super().__init__(color)
         self.radius = r
         self.diam = 2*self.radius
+        self.Opisan = Opisan
+        self.Vpisan = Vpisan
 
     def square(self):
         self.S = pow(self.radius,2)*self.pi
@@ -66,6 +76,10 @@ class Circle(Bez_Verh):
 
 class Ellips(Bez_Verh):
 
+    Focus_1 = [5,0]
+    Foucs_2 = [-5,0]
+    c = 5
+
     def info(self):
         print("Эллипс, наследник плоских фигур без вершин")
 
@@ -74,6 +88,9 @@ class Ellips(Bez_Verh):
         super().__init__(color)
         self.a = a
         self.b = b
+        self.c = math.sqrt(math.fabs(pow(self.a,2)-pow(self.b,2)))
+        self.Focus_1 = [self.c, 0]
+        self.Foucs_2 = [-self.c, 0]
 
     def square(self):
         self.S = self.a*self.b*self.pi
@@ -82,6 +99,9 @@ class Ellips(Bez_Verh):
     def perim(self):
         self.P = 2*self.pi*math.sqrt((pow(self.a, 2)+pow(self.b, 2))/2)
         print("Периметр (P) эллипса = ", self.P)
+
+    def focus(self):
+        print("Фокусное расстояние эллипса = ", self.c)
 
 
 class Parallel(S_Verh):
@@ -107,7 +127,14 @@ class Parallel(S_Verh):
 
 class Triangle(S_Verh):
 
-    def __init__(self, x1, y1, x2, y2, x3, y3,color):
+    a = 0
+    b = 0
+    c = 0
+    alpha = 0
+    beta = 0
+    gamma = 0
+
+    def __init__(self, x1, y1, x2, y2, x3, y3, color):
         print()
         super().__init__(color)
         self.A = {"x": x1, "y": y1}
@@ -116,12 +143,24 @@ class Triangle(S_Verh):
         self.a = math.sqrt((pow((self.B["x"] - self.C["x"]), 2)) + (pow((self.B["y"] - self.C["y"]), 2)))
         self.b = math.sqrt((pow((self.A["x"] - self.C["x"]), 2)) + (pow((self.A["y"] - self.C["y"]), 2)))
         self.c = math.sqrt((pow((self.B["x"] - self.A["x"]), 2)) + (pow((self.B["y"] - self.A["y"]), 2)))
+        self.beta = round(
+            math.degrees(math.acos((pow(self.a, 2) + pow(self.c, 2) - pow(self.b, 2)) / (2 * self.a * self.c))))
+        self.gamma = round(
+            math.degrees(math.acos((pow(self.a, 2) + pow(self.b, 2) - pow(self.c, 2)) / (2 * self.a * self.b))))
+        self.alpha = round(
+            math.degrees(math.acos((pow(self.b, 2) + pow(self.c, 2) - pow(self.a, 2)) / (2 * self.c * self.b))))
 
     def info(self):
         print("Треугольник, наследник плоских фигур с вершинами")
+        print("BC = ", self.a)
+        print("AC = ",self.b)
+        print("AB = ", self.c)
+        print("alpha = ", self.alpha)
+        print("beta = ", self.beta)
+        print("gamma = ", self.gamma)
 
     def square(self):
-        p=(self.a+self.b+self.c)/2
+        p = (self.a+self.b+self.c)/2
         self.S = math.sqrt(p*(p-self.a)*(p-self.b)*(p-self.c))
         print("Площадь (S) треугольника = ", self.S)
 
@@ -137,10 +176,15 @@ class Triangle(S_Verh):
 
 class Сylinder(Volumetric):
 
-    def __init__(self, height, radius, color):
+    kosoy = False
+    naklonnyi = False
+
+    def __init__(self, height, radius, color, kos=False, nakl=False):
         super().__init__(color)
         self.height = height
         self.radius = radius
+        self.kosoy = kos
+        self.naklonnyi = nakl
         print()
 
     def info(self):
@@ -163,13 +207,29 @@ class Сylinder(Volumetric):
 
 class Cube(Volumetric):
 
-    def __init__(self,height,color):
+    diag = 0
+    diag_grani = 0
+    vpis_sfera = False
+    opis_sfera = False
+
+    def __init__(self, height, color, vpis_sf=False, opis_sf=False):
         print()
         super().__init__(color)
+        self.diag = height*math.sqrt(3)
+        self.diag_grani = height * math.sqrt(2)
+        self.vpis_sfera = vpis_sf
+        self.opis_sfera = opis_sf
         self.height = height
 
     def info(self):
         print("Куб, наследник объемных фигур")
+        print("Диагональ куба = ", self.diag)
+        print("Диагональ грани куба = ", self.diag_grani)
+        if self.vpis_sfera == True:
+            print("У куба есть вписанная сфера")
+        if self.opis_sfera == True:
+            print("У куба есть сфера описанная около него")
+
 
     def s_poverh(self):
         self.S = 6*pow(self.height,2)
@@ -192,7 +252,7 @@ cyl.s_bok()
 cyl.vol()
 cyl.perim_bok()
 
-cub = Cube(15, "yellow")
+cub = Cube(15, "yellow",True)
 cub.info()
 cub.s_poverh()
 cub.vol()
@@ -209,15 +269,17 @@ parallelogram.info()
 parallelogram.square()
 parallelogram.perim()
 
-krug = Circle(5,"green")
+krug = Circle(5,"green",True)
 krug.info()
 krug.square()
 krug.perim()
 
-ellps = Ellips(13,4,"red")
+ellps = Ellips(13, 4, "red")
 ellps.info()
 ellps.square()
 ellps.perim()
+ellps.focus()
+
 
 
 
